73ff720be77b66ab2b3d083f8f3a035019cec948
xiaweizi <1012126908@qq.com>
2018-12-20 16:04:45 +0800

    [dev] 插入排序测试代码添加

--- src/sort/InsertSort.java
+++ src/sort/InsertSort.java
@@ -32,19 +32,36 @@ class InsertSort {
         }
     }
 
+    private void insertSortReverse(int[] data, int n) {
+        for (int i = 1; i < n; i++) {
+            int value = data[i];
+            int j = i - 1;
+            for (; j >= 0; j--) {
+                if (value > data[j]) {
+                    data[j+1] = data[j];
+                } else {
+                    break;
+                }
+            }
+            data[j+1] = value;
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] data = new int[]{1, 2, 3, 5, 6, 4};
         InsertSort insertSort = new InsertSort();
         insertSort.insertSort(data, data.length);
         System.out.println("result:\t" + Arrays.toString(data) + "  count:\t" + insertSort.count);
+        insertSort.insertSortReverse(data, data.length);
+        System.out.println("result:\t" + Arrays.toString(data) + "  count:\t" + insertSort.count);
 
-        long lastTime = System.currentTimeMillis();
-        int[][] value = ArrayCommon.createRandomData();
-        for (int i = 0; i < 200; i++) {
-            int[] temp = value[i];
-            insertSort.insertSort(temp, temp.length);
-        }
-        System.out.println("totalTime:\t" + (System.currentTimeMillis() - lastTime));
+//        long lastTime = System.currentTimeMillis();
+//        int[][] value = ArrayCommon.createRandomData();
+//        for (int i = 0; i < 200; i++) {
+//            int[] temp = value[i];
+//            insertSort.insertSort(temp, temp.length);
+//        }
+//        System.out.println("totalTime:\t" + (System.currentTimeMillis() - lastTime));
     }
 }
